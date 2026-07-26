33cf34ce78a8ff49b28fd5122d20c7685a831a70
oxfeeefeee <pb01005051@gmail.com>
2026-07-27 05:44:42 +0800

    feat: bundle browser-ready Voplay modules

--- apps/studio/src/components/home/content.ts
+++ apps/studio/src/components/home/content.ts
@@ -15,7 +15,7 @@ export type StarterAccent = 'blue' | 'violet' | 'mint' | 'amber';
 export type BlockKartAction = 'play' | 'source';
 
 export const BLOCKKART_GITHUB_URL = 'https://github.com/vo-lang/BlockKart';
-export const BLOCKKART_PLAY_URL = '/quickplay/blockkart-b361a1ff07a8a3f12c3155e6f209bb9924c1437c.tar.gz';
+export const BLOCKKART_PLAY_URL = '/quickplay/blockkart-95fecfafbd87d731f74ec7f5c1713f0d20d7452d.tar.gz';
 
 export interface StarterCategory {
   id: StarterCategoryId;
